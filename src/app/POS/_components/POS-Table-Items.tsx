eb97ce6de0277fc38f3a2f0708d096e2e68665f9
Iskyy <iskycruise01@gmail.com>
2023-09-08 09:19:12 +0700

    feat: Add function that total amount of product

--- src/app/POS/_components/POS-Table-Items.tsx
+++ src/app/POS/_components/POS-Table-Items.tsx
@@ -1,39 +1,56 @@
+"use client";
+
 import Image, { StaticImageData } from "next/image";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
+import { useEffect, useState } from "react";
 
 interface PosItemProps {
-  //   id: number;
+  id?: string;
+  type: string;
+  price: number;
   img: StaticImageData;
-  //   type: string;
 }
 
-export default function POSItems({ img }: PosItemProps) {
+export default function POSItems({ id, type, price, img }: PosItemProps) {
+  const [total, setTotal] = useState<number>(0);
+  const [clientGal, setClientGal] = useState<number>(0);
+  const [WRSGal, setWRSGal] = useState<number>(0);
+
+  useEffect(() => {
+    const qty = clientGal + WRSGal;
+    setTotal(price * qty);
+  }, [clientGal, WRSGal, price]);
+
   return (
     <>
-      <div className="grid grid-cols-9 gap-x-1 place-content-center text-center py-2 ">
+      <div className="grid grid-cols-8 gap-x-1 place-content-center text-center py-2 ">
         <h5 className="text-sm ">1</h5>
         <div className="text-sm col-span-2 flex items-center gap-x-2">
           <Image src={img} alt="Slim " height={25} className="" />
-          <p>Slim</p>
+          <p>{type}</p>
         </div>
-        <h5 className="text-sm">20.00</h5>
+        <h5 className="text-sm">{price.toFixed(2)}</h5>
         <input
           type="number"
           min={0}
+          value={clientGal}
+          onChange={(e) => setClientGal(parseFloat(e.target.value))}
           className="outline-none text-sm text-center"
         />
         <input
           type="number"
           min={0}
+          value={WRSGal}
+          onChange={(e) => setWRSGal(parseFloat(e.target.value))}
           className="outline-none text-sm text-center"
         />
-        <input
+        {/* <input
           type="number"
           min={0}
           className="outline-none text-sm text-center"
-        />
-        <h5 className="text-sm">1000.00</h5>
+        /> */}
+        <h5 className="text-sm">{total.toFixed(2)}</h5>
 
         <Checkbox className="mx-auto " />
       </div>
--- src/app/POS/page.tsx
+++ src/app/POS/page.tsx
@@ -11,8 +11,27 @@ import { POSSelectCustomerBTN } from "./_components/POS-Select-Customer-BTN";
 import POSModalReturnGallon from "./_components/POS-Modal-Return-Gallon";
 import AddNewCustomerModal from "@/components/Add-Customer/Add-Customer-Modal";
 import { PaymentModal } from "./_components/POS-Modal-Payment";
+import { StaticImageData } from "next/image";
+import { ItemsGallon } from "@/utils/Products-data/items";
+import { ItemsBottle } from "@/utils/Products-data/items";
 
-export default function POS_Page() {
+interface PosItemProps {
+  id?: string;
+  type: string;
+  price: number;
+  img: StaticImageData;
+}
+
+const GetItems = async () => {
+  const Data: PosItemProps[] = await ItemsGallon.map((data) => {
+    return data;
+  });
+  return Data;
+};
+
+export default async function POS_Page() {
+  const items = await GetItems();
+
   return (
     <>
       <POSSearchUserModal />
@@ -43,26 +62,46 @@ export default function POS_Page() {
                   </TabsList>
 
                   <div className="bg-slate-100 w-full">
-                    <header className="h-8 grid grid-cols-9 gap-x-1 place-content-center text-center font-semibold bg-sky-500">
+                    <header className="h-8 grid grid-cols-8 gap-x-1 place-content-center text-center font-semibold bg-sky-500">
                       <h4 className="text-sm ">#</h4>
                       <h4 className="text-sm col-span-2">ITEM</h4>
                       <h4 className="text-sm">PRICE</h4>
                       <h4 className="text-sm">CLI-GAL</h4>
                       <h4 className="text-sm">WRS-GAL</h4>
-                      <h4 className="text-sm">FREE</h4>
+                      {/* <h4 className="text-sm">FREE</h4> */}
                       <h4 className="text-sm">TOTAL</h4>
                       <h4 className="text-sm ">BUY</h4>
                     </header>
                     {/* Gallon TAB*/}
                     <TabsContent value="gallon">
-                      <POSItems img={Slim} />
-                      <POSItems img={Slim} />
+                      {items.map((item) => {
+                        return (
+                          <>
+                            <POSItems
+                              key={item.id}
+                              type={item.type}
+                              price={item.price}
+                              img={item.img}
+                            />
+                          </>
+                        );
+                      })}
                     </TabsContent>
 
                     {/* BOTTLE TAB */}
                     <TabsContent value="bottle">
-                      <POSItems img={Rounded} />
-                      <POSItems img={Rounded} />
+                      {ItemsBottle.map((item: PosItemProps) => {
+                        return (
+                          <>
+                            <POSItems
+                              key={item.id}
+                              type={item.type}
+                              price={item.price}
+                              img={item.img}
+                            />
+                          </>
+                        );
+                      })}
                     </TabsContent>
                   </div>
                 </Tabs>
