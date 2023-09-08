ec2b7a153b1f7a3112dba26a2387b1de0eed4bbd
Iskyy <iskycruise01@gmail.com>
2023-09-08 09:18:30 +0700

    Add Products Fake Data

---
+++ src/utils/Products-data/items.ts
@@ -0,0 +1,44 @@
+import Slim from "@/assets/items_img/slim_gallon.png";
+import Round from "@/assets/items_img/rounded_gallon.png";
+import bottle from "@/assets/items_img/bottles.png";
+
+export const ItemsGallon = [
+  {
+    id: "1201",
+    type: "Slim",
+    price: 20,
+    img: Slim,
+  },
+  {
+    id: "1202",
+    type: "Slim",
+    price: 25,
+    img: Round,
+  },
+];
+export const ItemsBottle = [
+  {
+    id: "1301",
+    type: "1L",
+    price: 10,
+    img: bottle,
+  },
+  {
+    id: "1302",
+    type: "2L",
+    price: 15,
+    img: bottle,
+  },
+  {
+    id: "1303",
+    type: "3L",
+    price: 20,
+    img: bottle,
+  },
+  {
+    id: "1304",
+    type: "4L",
+    price: 25,
+    img: bottle,
+  },
+];
