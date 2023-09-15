110c365d008a011d04a6e3755d2a2f49c89af824
Iskyy <iskycruise01@gmail.com>
2023-09-15 09:32:07 +0700

    refactor: Modify file name

---
+++ src/app/Monitoring/Last-Gallon-Return-Column.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { IUser } from "../../../typings";
+import { DataTableRowActions } from "../Customer/data-table-row-action";
+import { DataTableNameColumn } from "@/components/react-table/Data-Table-Name-Column";
+
+export const LastGallonReturnColumns: ColumnDef<any>[] = [
+  {
+    header: "Name",
+    accessorKey: "fullname",
+    cell: ({ row }) => <DataTableNameColumn row={row} />,
+  },
+  {
+    header: "Gallom",
+    accessorKey: "Alias",
+  },
+
+  {
+    header: "Last Return",
+    cell: ({ row }) => new Date().toDateString(),
+  },
+  {
+    id: "action",
+    cell: ({ row }) => <DataTableRowActions row={row} />,
+  },
+];
