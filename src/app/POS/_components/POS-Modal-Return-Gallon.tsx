c628e24c0acccd86d2a57f60c3ab7b50be8cffa4
Iskyy <iskycruise01@gmail.com>
2023-09-17 18:46:17 +0700

    refactor: Remove x icon on return modal and replace it with button and Add save button

--- src/app/POS/_components/POS-Modal-Return-Gallon.tsx
+++ src/app/POS/_components/POS-Modal-Return-Gallon.tsx
@@ -3,13 +3,12 @@
 import React from "react";
 import Image from "next/image";
 import Slim from "@/assets/items_img/slim_gallon.png";
-import { IoClose } from "react-icons/io5";
 import POSBTNHeaderStore from "@/lib/zustand/POSPage-store/BTN-header";
+import { Button } from "@/components/ui/button";
 
 export default function POSModalReturnGallon() {
   const { showReturnGallon, toggleShowReturn } = POSBTNHeaderStore();
 
-  const show = true;
   return (
     <>
       <section
@@ -19,16 +18,9 @@ export default function POSModalReturnGallon() {
       >
         <div className="h-max w-full flex items-center justify-center  py-4 ">
           <main className="relative  max-w-4xl w-[100%] p-4 bg-white dark:bg-dark_bg border border-gray-900/10 shadow-md rounded ">
-            <div className="flex justify-between ">
-              <h1 className="mb-4 font-semibold text-gray-900  ml-4 relative before:absolute before:-left-3 before:h-full before:w-2 before:bg-red-400">
-                Return Gallon
-              </h1>
-              <IoClose
-                size={22}
-                className="place-self-start align-top cursor-pointer text-gray-500"
-                onClick={() => toggleShowReturn(false)}
-              />
-            </div>
+            <h1 className="mb-4 font-semibold text-gray-900  ml-4 relative before:absolute before:-left-3 before:h-full before:w-2 before:bg-red-400">
+              Return Gallon
+            </h1>
 
             <div className="">
               <div className="bg-sky-500 font-semibold grid grid-cols-6 items-center place-content-center place-items-center">
@@ -83,6 +75,24 @@ export default function POSModalReturnGallon() {
                   className=" text-sm text-center outline-none border border-gray-500 py-1 w-16 "
                 />
               </div>
+            </div>
+
+            {/* BUTTON FOOTER */}
+            <div className=" flex justify-end space-x-4 mt-8">
+              <Button
+                variant="outline"
+                type="button"
+                // className="bg-red-500 hover:bg-red-600"
+                onClick={() => toggleShowReturn(false)}
+              >
+                Close
+              </Button>
+              <Button
+                type="submit"
+                // className="bg-blue-500 hover:bg-blue-600"
+              >
+                Return
+              </Button>
             </div>
           </main>
         </div>
