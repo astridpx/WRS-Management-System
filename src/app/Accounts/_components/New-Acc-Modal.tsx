bc9bbc9781f6a07019154aaa3307e6e66c73c1b5
Iskyy <iskycruise01@gmail.com>
2023-10-16 18:47:35 +0700

    feat: Add create new accounts

--- src/app/Accounts/_components/New-Acc-Modal.tsx
+++ src/app/Accounts/_components/New-Acc-Modal.tsx
@@ -4,6 +4,14 @@ import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { BiShow, BiHide } from "react-icons/bi";
+import { createNewAccount } from "../services/api";
+import {
+  SuccessToast,
+  ErrorToast,
+  LoadingToast,
+  DissmissToast,
+} from "@/components/Toast/toast";
+import { useMutation, useQueryClient } from "react-query";
 
 interface IAccModal {
   modal: boolean;
@@ -11,8 +19,60 @@ interface IAccModal {
 }
 
 export function NewAccModal({ modal, toggleModal }: IAccModal) {
+  const queryClient = useQueryClient();
   const [showPass, setShowPass] = useState<boolean>(false);
   const [showCPass, setShowCPass] = useState<boolean>(false);
+  const [data, setData] = useState({
+    first_name: "",
+    last_name: "",
+    username: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+
+  const { mutateAsync } = useMutation({
+    mutationFn: createNewAccount,
+    onMutate: () => {
+      toggleModal(false);
+      LoadingToast("Add new customer is pending...");
+    },
+    onSuccess: (data) => {
+      DissmissToast();
+      SuccessToast(data?.message);
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      setData({
+        first_name: "",
+        last_name: "",
+        username: "",
+        email: "",
+        password: "",
+        cpassword: "",
+      });
+    },
+    onError: (error: any) => {
+      DissmissToast();
+      ErrorToast(error?.response?.data?.message);
+    },
+  });
+
+  // HANDLE INPUT CHANGE
+  const handlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // HANDLE SUBMIT
+  const HandleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (data.password !== data.cpassword)
+      return ErrorToast("Password didn't match.");
+
+    await mutateAsync({ ...data });
+  };
 
   return (
     <>
@@ -22,7 +82,10 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
         } h-screen w-full overflow-y-auto bg-black/75 bg-opacity-95 absolute z-20  `}
       >
         <div className="h-max w-full  flex items-center justify-center py-4   ">
-          <main className="relative  max-w-4xl w-[100%] p-4 bg-white dark:bg-dark_bg border border-gray-900/10 shadow-md rounded">
+          <form
+            onSubmit={(e) => HandleSubmit(e)}
+            className="relative  max-w-4xl w-[100%] p-4 bg-white dark:bg-dark_bg border border-gray-900/10 shadow-md rounded"
+          >
             <h2 className="text-base font-semibold leading-7 text-gray-900 ml-3 relative before:absolute before:-left-3 before:h-full before:w-2 before:bg-red-400">
               Create New Account
             </h2>
@@ -40,7 +103,9 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                     type="text"
                     name="first_name"
                     required
+                    value={data.first_name}
                     placeholder="Enter your first name"
+                    onChange={(e) => handlChange(e)}
                   />
                 </div>
               </div>
@@ -58,6 +123,8 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                     name="last_name"
                     required
                     placeholder="Enter your last name"
+                    value={data.last_name}
+                    onChange={(e) => handlChange(e)}
                   />
                 </div>
               </div>
@@ -75,6 +142,8 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                     name="username"
                     required
                     placeholder="Enter your username"
+                    value={data.username}
+                    onChange={(e) => handlChange(e)}
                   />
                 </div>
               </div>
@@ -92,6 +161,8 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                     name="email"
                     required
                     placeholder="Enter your email"
+                    value={data.email}
+                    onChange={(e) => handlChange(e)}
                   />
                 </div>
               </div>
@@ -113,9 +184,11 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                   </div>
                   <Input
                     type={showPass ? "text" : "password"}
-                    name="pass"
+                    name="password"
                     required
                     placeholder="Enter your password"
+                    value={data.password}
+                    onChange={(e) => handlChange(e)}
                   />
                 </div>
               </div>
@@ -135,9 +208,11 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                   </div>
                   <Input
                     type={showCPass ? "text" : "password"}
-                    name="cpass"
+                    name="cpassword"
                     required
                     placeholder="Confirm password"
+                    value={data.cpassword}
+                    onChange={(e) => handlChange(e)}
                   />
                 </div>
               </div>
@@ -150,6 +225,14 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                 type="button"
                 onClick={() => {
                   toggleModal(false);
+                  setData({
+                    first_name: "",
+                    last_name: "",
+                    username: "",
+                    email: "",
+                    password: "",
+                    cpassword: "",
+                  });
                 }}
 
                 // className="bg-red-500 hover:bg-red-600"
@@ -163,7 +246,7 @@ export function NewAccModal({ modal, toggleModal }: IAccModal) {
                 Save
               </Button>
             </div>
-          </main>
+          </form>
         </div>
       </section>
     </>
--- src/app/Accounts/services/api.ts
+++ src/app/Accounts/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { format } from "date-fns";
 
 export const getAllAccounts = async () => {
   const { data } = await axios.get("/api/accounts");
@@ -7,10 +8,19 @@ export const getAllAccounts = async () => {
     const newData = {
       ...d,
       fullname: `${d.first_name} ${d.last_name}`,
+      last_active: format(new Date(d.last_active), "LLL dd, y"),
     };
 
     return newData;
   });
 
   return Data;
+};
+
+export const createNewAccount = async (Data: any) => {
+  const { data } = await axios.post("/api/accounts", {
+    ...Data,
+  });
+
+  return data;
 };
