aa5b8f77f72db3ef3b7fcc500b911138cf958790
Iskyy <iskycruise01@gmail.com>
2023-10-24 18:18:27 +0700

    fix erorr

--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -8,14 +8,14 @@ export async function GET(req: NextRequest) {
   console.log(res.data);
   console.log(res.data.ip);
 
-  const apiURL = "https://ipgeolocation.abstractapi.com/v1/";
-  const apiKey = "28bc253a-ed77-4a93-98b8-cc252c6e7c0b";
+  // const apiURL = "https://ipgeolocation.abstractapi.com/v1/";
+  // const apiKey = "28bc253a-ed77-4a93-98b8-cc252c6e7c0b";
 
-  const url =
-    "https://apiip.net/api/check?ip=" + res.data.ip + "&accessKey=" + apiKey;
+  // const url =
+  //   "https://apiip.net/api/check?ip=" + res.data.ip + "&accessKey=" + apiKey;
 
-  const response = await axios.get(url);
-  const result = response.data;
+  // const response = await axios.get(url);
+  // const result = response.data;
 
-  return NextResponse.json({ ip: res.data.ip, result: result });
+  return NextResponse.json({ ip: res.data.ip });
 }
