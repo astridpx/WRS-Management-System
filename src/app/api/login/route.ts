f7154c083cdab48208581d9fade3973b46c794fd
Iskyy <iskycruise01@gmail.com>
2023-10-29 14:14:34 +0700

    Fixing IpGeolocation

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -37,27 +37,43 @@ export async function POST(req: Request) {
     // const res = await axios.get("https://api.ipify.org/?format=json");
     // console.log(res.data.ip);
 
-    // ABSTRACT API
-    const { data: ipData } = await axios.get(
-      "https://ipgeolocation.abstractapi.com/v1/?api_key=8fa7278f039e4c828bc7311219715d0a"
-    );
-    console.log(ipData);
+    // IPFIND
+    // const { data: ipData } = await axios.get(
+    //   "https://api.ipfind.com?ip=8.8.8.8&auth=8bfec4d9-c47d-425e-8d11-569f93a0dc54"
+    //   );
+    //   console.log(ipData);
 
-    await Acc.findOneAndUpdate(
-      { username },
-      {
-        last_active: new Date(),
-        $push: {
-          login_history: {
-            isDesktop,
-            deviceName,
-            ip: ipData.ip_address,
-            date: new Date(),
-            address: `${ipData.city}, ${ipData.region} - ${ipData.country_code}`,
-          },
-        },
-      }
-    );
+    // ABSTRACT API
+    await axios
+      .get(
+        "https://ipgeolocation.abstractapi.com/v1/?api_key=8fa7278f039e4c828bc7311219715d0a"
+      )
+      .then(async (ipData: any) => {
+        console.log(ipData.data);
+        
+        const x = await Acc.findOneAndUpdate(
+          { username },
+          {
+            last_active: new Date(),
+            $push: {
+              login_history: {
+                isDesktop: isDesktop ? isDesktop : "Unknown",
+                deviceName: deviceName ? deviceName : "Unknown",
+                ip: ipData.data.ip_address,
+                date: new Date(),
+                address: `${ipData.data.city}, ${ipData.data.region} - ${ipData.data.country_code}`,
+              },
+            },
+          }
+        );
+
+        if (x) {
+          console.log("login logged");
+        }
+      })
+      .catch((err) => console.log(err));
+
+    // console.log(ipData);
 
     // Create a new user object without the hash_password property
     const sanitizedUser = { ...user.toObject() };
