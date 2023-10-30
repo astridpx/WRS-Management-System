9c038545046b37f67118cb2663f7151088b0b5e9
Iskyy <iskycruise01@gmail.com>
2023-10-30 15:06:07 +0700

    style: Change login page ui

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,72 +1,48 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
-import peoples from "../../assets/people-cofee.svg";
-import bg from "../../assets/bg-login.jpg";
-import blob from "../../assets/login-form-blob.png";
+import h2o from "@/assets/h2o.jpg";
+import waterbottles from "@/assets/water-bottles.jpg";
+import wave2 from "@/assets/wave2.png";
 import LoginForm from "./Login-Form";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { UserStore } from "@/lib/zustand/User/user.store";
-import { useRouter } from "next/navigation";
-import {
-  SuccessToast,
-  ErrorToast,
-  LoadingToast,
-  DissmissToast,
-  InfoToast,
-} from "@/components/Toast/toast";
 
 export default function LoginPage() {
-  const { clearUser, setUser } = UserStore();
-  const router = useRouter();
-  const { data: session, status, update } = useSession();
-  const [loading, setLoading] = useState(false);
-
-  console.log(session);
-
-  useEffect(() => {
-    if (session) {
-      const User = session.user;
-      setUser({ ...User });
-      setLoading(false);
-      DissmissToast();
-      InfoToast("Data Successfully Initialized.");
-      router.push("/Dashboard");
-    }
-  }, [router, session, setUser]);
-
   return (
     <>
-      <main className="h-screen w-full ">
-        <section className="min-h-screen h-screen w-full grid grid-cols-2 relative">
+      <main className="h-screen w-full grid grid-cols-5">
+        <section className="col-span-3 relative">
           <Image
-            src={bg}
-            alt="bg"
+            // src={falls}
+            // src={waterbottles}
+            src={h2o}
+            // src={glassWinter}
+            alt="bg-login"
+            height={300}
+            width={250}
             unoptimized
-            className="absolute bg-cover bg-center bg-no-repeat h-screen w-full -z-10 opacity-90s blsur-sm "
+            className="h-full w-full object-cover "
           />
-          <div className=" flex items-center  ">
+          <div className="flex items-start justify-end h-full w-full top-0 overflow-hidden bg-blue-600/70 absolute">
             <Image
-              src={peoples}
-              alt="people"
+              src={wave2}
+              alt="bg-login"
+              height={300}
+              // height={700}
+              width={600}
               unoptimized
-              className="bg-cover bg-center bg-no-repeat  "
+              className="h-full w-[35rem] -rostate-90"
             />
           </div>
+        </section>
 
-          {/* Login form */}
-          <div className="flex justify-center items-center  ">
-            <Image
-              src={blob}
-              alt="blob"
-              // height={300}
-              // width={300}
-              unoptimized
-              className="absolute bg-center h-[33rem] w-[33rem] -z-10 opacity-75 blur-sm "
-            />
-            <LoginForm loading={loading} setLoading={setLoading} />
+        <section className="col-span-2  bg-white flex justify-center items-center ">
+          <div className="p-4 w-[80%] space-y-28">
+            <header className="text-4xl font-semibold">
+              <h2 className=" text-dark_blue">Hello,</h2>
+              <h2 className=" text-light_blue">Welcome Back :)</h2>
+            </header>
+
+            {/* FORM HERE */}
+            <LoginForm />
           </div>
         </section>
       </main>
