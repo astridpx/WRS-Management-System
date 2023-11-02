d730df6944b694eec4b5202ade883b6672737fa4
Iskyy <iskycruise01@gmail.com>
2023-11-02 08:46:41 +0700

    Order the response data from newest to oldest

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -4,7 +4,7 @@ import { Notif } from "@/lib/mongodb/model/Notifications.model";
 
 //  @desc GET All NOTIFICATIONS
 export async function GET() {
-  const notification = await Notif.find().sort({ date: 1 }).lean();
+  const notification = await Notif.find().sort({ date: -1 }).lean();
 
   return NextResponse.json({ data: notification }, { status: 200 });
 }
